ed990c9ed48b510682b0b1a2cfab5d87e5137859
tabitha zachariah <tabyzachariah7@gamil.com>
2026-03-11 14:04:52 +0300

    main.c

---
+++ main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main() {
+    char firstname[50], surname[50];
+    char input[20];
+    int score;
+    char grade;
+
+    printf("Enter firstname and surname: ");
+    scanf("%s %s", firstname, surname);
+
+    getchar();   // clears leftover newline
+
+    printf("Enter score: ");
+    fgets(input, sizeof(input), stdin);
+
+    if (input[0] == '\n') {
+        grade = 'Y';
+        score = 0;
+    }
+    else {
+        score = atoi(input);
+
+        if (score < 0 || score > 100)
+            grade = 'X';
+        else if (score < 40)
+            grade = 'F';
+        else if (score < 50)
+            grade = 'E';
+        else if (score < 60)
+            grade = 'D';
+        else if (score < 70)
+            grade = 'C';
+        else if (score < 80)
+            grade = 'B';
+        else
+            grade = 'A';
+    }
+
+    printf("%s %s %d Grade:%c\n", firstname, surname, score, grade);
+
+    return 0;
+}

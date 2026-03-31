95a53b6c131953356f9d2106119751fe81609853
tabitha zachariah <tabyzachariah7@gamil.com>
2026-03-31 22:04:04 +0300

    Fix proper indentation

--- main.c
+++ main.c
@@ -16,44 +16,44 @@ int main() {
         return 1;
     }
     while(more == 'y' || more == 'Y') {
-    printf("Enter firstname and surname: ");
-    scanf("%s %s", firstname, surname);
+        printf("Enter firstname and surname: ");
+        scanf("%s %s", firstname, surname);
 
-    getchar();   // clears leftover newline
+        getchar();   // clears leftover newline
 
-    printf("Enter score: ");
-    fgets(input, sizeof(input), stdin);
+        printf("Enter score: ");
+        fgets(input, sizeof(input), stdin);
 
-    if (input[0] == '\n') {
-        grade = 'Y';
-        score = 0;
-    }
-    else {
-        score = atoi(input);
+        if (input[0] == '\n') {
+            grade = 'Y';
+            score = 0;
+        }
+        else {
+            score = atoi(input);
 
-        if (score < 0 || score > 100)
-            grade = 'X';
-        else if (score < 40)
-            grade = 'F';
-        else if (score < 50)
-            grade = 'E';
-        else if (score < 60)
-            grade = 'D';
-        else if (score < 70)
-            grade = 'C';
-        else if (score < 80)
-            grade = 'B';
-        else
-            grade = 'A';
-    }
+            if (score < 0 || score > 100)
+                grade = 'X';
+            else if (score < 40)
+                grade = 'F';
+            else if (score < 50)
+                grade = 'E';
+            else if (score < 60)
+                grade = 'D';
+            else if (score < 70)
+                grade = 'C';
+            else if (score < 80)
+                grade = 'B';
+            else
+                grade = 'A';
+        }
 
-    printf("%s %s %d Grade:%c\n", firstname, surname, score, grade);
+        printf("%s %s %d Grade:%c\n", firstname, surname, score, grade);
 
-    // ---NEW: write to file ---
-    fprintf(fp, "%s %s %d Grade:%c\n", firstname, surname, score, grade);
+        // ---NEW: write to file ---
+        fprintf(fp, "%s %s %d Grade:%c\n", firstname, surname, score, grade);
 
-    printf("Do you want to enter another student? (y/n): ");
-    scanf(" %c", &more);
+        printf("Do you want to enter another student? (y/n): ");
+        scanf(" %c", &more);
     }
     fclose(fp); //close file
     printf("All grades saved to grades.txt!\n");
